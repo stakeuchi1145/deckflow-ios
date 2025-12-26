fbfc971bb4e3251964cde4a6816a8253b0ad7e0b
stakeuchi1145 <shintyakos@engineer-life.com>
2025-12-26 22:38:01 +0900

    Initial Commit

---
+++ deckflow-ios/deckflow_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  deckflow_iosApp.swift
+//  deckflow-ios
+//
+//  Created by shin takeuchi on 2025/12/26.
+//
+
+import SwiftUI
+
+@main
+struct deckflow_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
