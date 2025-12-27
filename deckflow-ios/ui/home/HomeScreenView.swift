3c1f984202eb6538cd09622b87324ebd9eacc0e5
stakeuchi1145 <shintyakos@engineer-life.com>
2025-12-27 22:54:09 +0900

    ホーム画面追加

--- deckflow-ios/ui/home/HomeScreenView.swift
+++ deckflow-ios/ui/home/HomeScreenView.swift
@@ -8,10 +8,47 @@
 import SwiftUI
 
 struct HomeScreenView: View {
+    @State var searchCardText: String = ""
+
     var body: some View {
         ZStack {
             VStack {
-                Text("ホーム画面")
+                ZStack {
+                    Text("My Cards")
+                        .font(.system(size: 32))
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+
+                    Button(action: {}) {
+                        Image(systemName: "line.3.horizontal")
+                            .resizable()
+                            .frame(width: 14, height: 14)
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .padding()
+                    }
+                }
+                .frame(maxWidth: .infinity)
+
+                HStack {
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .padding(.leading, 8)
+
+                        TextField("検索", text: $searchCardText)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.vertical, 8)
+                    .border(Color.gray, width: 1)
+
+                    Button(action: {}) {
+                        Text("Filter")
+                            .foregroundColor(.white)
+                            .padding(10)
+                            .background(Color(hex: "#FF2196F3"))
+                            .cornerRadius(20)
+                    }
+                }
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
             .padding()
